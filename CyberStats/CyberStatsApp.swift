8348a853728ee62b389eb194327575585892898a
bekiosi12-boop <bekiosi12@icloud.com>
2026-04-10 16:49:18 +0300

    Initial Commit

---
+++ CyberStats/CyberStatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CyberStatsApp.swift
+//  CyberStats
+//
+//  Created by Osmanov's macbook on 4/10/26.
+//
+
+import SwiftUI
+
+@main
+struct CyberStatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
